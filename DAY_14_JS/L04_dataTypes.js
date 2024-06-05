b43a450128ce5fc6a9691fe4d062be2f13750c64
Abhay Gupta <ab04012003@gmail.com>
2024-06-05 06:40:58 +0530

    data types in JS file is added

---
+++ DAY_14_JS/L04_dataTypes.js
@@ -0,0 +1,14 @@
+// Data types in js 
+
+// primitive types 
+
+// 1. Number
+// 2. Boolean
+// 3. String
+// 4. Undefined
+// 5. Null
+
+// 6. Bigint
+// 7. Symbol
+
+// To check the type of variable use :-- typeof
