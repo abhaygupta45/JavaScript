77266824bb91290b1dc2f7329fea0268b23a2ee6
Abhay Gupta <ab04012003@gmail.com>
2024-06-05 06:42:10 +0530

    operations in JS file is added

---
+++ DAY_14_JS/L06_operations.js
@@ -0,0 +1,23 @@
+// a = 20 
+// b = 10
+
+// addition
+// sum = a+b;
+
+// subtraction
+// diff = a -b 
+
+// multiplication 
+// pro = a*b 
+
+// division
+// a/b
+
+// modulo
+// rem = a % b;
+
+// modulo (reminder operator)
+// 12 % 5 = 2
+
+// Ecponetiation (power operator)
+// 2**3 = 8
