c74ec9f74115b33e39873deb140fa3c48e4dcdfa
Abhay Gupta <ab04012003@gmail.com>
2024-06-06 07:26:25 +0530

    practice question is added

---
+++ DAY_14_JS/L09_practiceQuestion.js
@@ -0,0 +1,18 @@
+// Note .. You can practice these questions in console.
+
+// Q-1. What is the value of age after this code runs?
+
+// let age = 23 ;
+// age = 2 ; // after two years
+
+
+// output = 23
+
+// Q-2. What is the value of avg after this code runs?
+
+// let hindi = 80;
+// let eng = 90;
+// let math = 100;
+// let avg = (hindi + eng + math)/3;
+
+// output = 90
