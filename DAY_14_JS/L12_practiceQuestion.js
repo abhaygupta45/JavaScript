43d38d6c2c19761f257a67572651c8955c931082
Abhay Gupta <ab04012003@gmail.com>
2024-06-07 12:25:04 +0530

    practice question file is added

---
+++ DAY_14_JS/L12_practiceQuestion.js
@@ -0,0 +1,11 @@
+// note - solve this question in console
+
+// Q-1. What is the value of each variable in each line of code?
+
+// let num = 5;
+// let newNum = num++;
+// newNum = ++num;
+
+// output - line 1 5
+            // line 2 5 , 6
+            // line 3 7 , 7
