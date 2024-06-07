8dd42061c97b454d20ce81c42a1143b1a4faedc1
Abhay Gupta <ab04012003@gmail.com>
2024-06-07 12:24:30 +0530

    unary operator file is added

---
+++ DAY_14_JS/L11_unaryOperators.js
@@ -0,0 +1,20 @@
+// Unary Operators
+
+// Increment operator
+
+// let age = 10
+// age = age + 1;
+// age += 1;
+
+// newAge = age++ // post-increment
+// newAge = ++age // pre-increment
+
+// Decrement operator
+
+// let age = 10;
+// age = age - 1;
+// age -= 1
+
+//  newAge = age-- // post decrement
+// newAge = --age // pre decrement
+
