3647b5d9eba07de130b1962af7b262c2365b53fc
Abhay Gupta <ab04012003@gmail.com>
2024-06-08 17:49:04 +0530

    identifer rules file is added

---
+++ DAY_14_JS/L13_identifierRules.js
@@ -0,0 +1,19 @@
+// Identifier Rules.
+
+// All javaScript variables must be identified with unique names (identifiers).
+
+// 1. Names can contain letters, digits, underscores, and dollar signs. (no space).
+// 2. Names must begin with letter. 
+// 3. Names can also begin with $ and _.
+// 4. Names are case sensitive (y and Y are differnet variables).
+// 5. Reserved words (like JavaScript, keywords) CAN NOT be used as names.
+
+// camelCase
+
+// Way of writing identifiers
+
+// camelCase (js naming convention)
+
+// snake case (all small letters)
+
+// PascalCase (all words start with capital letter.)
