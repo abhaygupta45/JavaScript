bcbad23c8c81f88e1630c83b4650d37204112079
Abhay Gupta <ab04012003@gmail.com>
2024-06-09 14:14:34 +0530

    string Indices file is added

---
+++ DAY_14_JS/L18_stringIndices.js
@@ -0,0 +1,23 @@
+// String Indices
+
+// let name = "TONY STARK";
+
+// T O N Y   S T A R K
+// _ _ _ _ _ _ _ _ _ _
+// 0 1 2 3 4 5 6 7 8 9
+
+
+// name[2] - it is used to access index
+
+// name.length - it is used to find the length of variable.
+
+// name[name.length-2];
+// output - 'R'
+
+
+// Concatenation 
+// Adding strings togeather.
+
+// "tony" + "stark" = "tony stark"
+
+// "tony" + 1 = "tony1"
