307b06ee571acc2764138e5023ad1d3e284cde49
Abhay Gupta <ab04012003@gmail.com>
2024-06-09 14:12:16 +0530

    practice question file is added

---
+++ DAY_14_JS/L16_practiceQuestion.js
@@ -0,0 +1,22 @@
+// Practice Questions
+
+// Note - You can solve this question in console.
+
+// Q-1. Find the errors in the following code ?
+
+// 1.
+//  let 1age = 5;
+//  let 2age = 10;
+
+// error - variable can not start with number.
+
+// 2. 
+//  let marks = 75;
+//  let isPass = True;
+
+// error = T is capital
+
+// 3. let isPass = 'true'
+
+// code will execute easily but this is not the correct way to declreare boolean value
+
