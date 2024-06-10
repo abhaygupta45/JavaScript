2e49a06546121c229eaa2d2fd1ecbe6e0e09b5f4
Abhay Gupta <ab04012003@gmail.com>
2024-06-10 09:51:26 +0530

    null and undefined file in js is added

---
+++ DAY_14_JS/L19_nullAndUndefined.js
@@ -0,0 +1,22 @@
+// null and undefined in JS
+
+// undefined
+// A variable that has not been assigned a value is of type undefined
+
+// let a;
+// <. undefined
+
+// > a
+// <. undefined
+
+null
+
+// the null value represents the intentional absence of any obeject value.
+
+// to be explicitly assigned.
+
+// let a = null;
+// <. undefined
+
+// > a 
+// <. null
