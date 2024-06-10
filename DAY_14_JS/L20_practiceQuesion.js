de4fb96dfc549dc13d2488ead3d75e3a7257768f
Abhay Gupta <ab04012003@gmail.com>
2024-06-10 09:52:24 +0530

    practice question file is added

---
+++ DAY_14_JS/L20_practiceQuesion.js
@@ -0,0 +1,39 @@
+// Practice Questions.
+
+// 1. Declare your name as a string and print its length in JS.
+
+// solution
+// let name = "Abhay Gupta"
+// typeof name
+// name.length
+
+// 2. Declare your first name as a string and print its first character.
+
+// solution
+// let firstName = "Abhay";
+// firstName[0]
+// output - 'A'
+
+// 3. Declare your first name as a striing and print its last character.
+
+// firstName.length
+// firstName[4]
+
+// firstName[firstName.length-1]
+
+// output - 'a'
+
+// 4.What is the output of the following code.
+// "apnacollege"+123
+
+// solutio
+// "apnacollege123"
+
+// 5. What are lengths of an empty string and a string with a single space?
+
+// solution
+
+// let space = " ";
+// space.length
+
+// output - 1
