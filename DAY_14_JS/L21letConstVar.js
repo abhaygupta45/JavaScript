94cac0c425ba46e80cd5cdf8a98cdcb6ca276afd
Abhay Gupta <ab04012003@gmail.com>
2024-06-10 09:53:02 +0530

    let const

---
+++ DAY_14_JS/L21letConstVar.js
@@ -0,0 +1,28 @@
+// let keyword
+
+// syntax of declaring variables.declaring
+// let age = 23;
+// age = age + 1;
+// let cgpa = 8.9;
+// cgpa = cgpa * 10
+
+// let num1 = 1;
+// let num2 = 2;
+
+// let finalSum = num1 + num2
+
+
+
+// const keyword
+
+// values of constants can't be changed with re-assignment and they can't be re-declared
+
+// const year = 2025;
+
+// year = year + 1 // error
+
+// const pie = 3.14;
+
+
+// var keyword
+// old syntax of writing variable.
