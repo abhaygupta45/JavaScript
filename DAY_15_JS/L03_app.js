9842f83b027733ff2ae218cd73491fc44335b0e1
Abhay Gupta <ab04012003@gmail.com>
2024-06-13 10:01:33 +0530

    js file of L03 is added

---
+++ DAY_15_JS/L03_app.js
@@ -0,0 +1,24 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+// console.log("the total price is :", pencilPrice + erasorPrice, "Rupees." );
+
+// let output = "The total price is :" +(pencilPrice + erasorPrice) + "rupees. ";
+
+// template Literals
+// let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`
+// console.log(output) 
+
+
+
+// templete literals ``
+console.log(`The total price is : ${pencilPrice + erasorPrice} Rupees.`);
+
+
+
+// Templete Literals 
+
+// they care used to add embedded expresions in a string.
+
+let a = 5;
+let b = 10;
+console.log(`Your pay ${a+b} rupees`);
