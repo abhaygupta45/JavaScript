a0e2d72e89f29d94618cb4da0045ecbefaf2eead
Abhay Gupta <ab04012003@gmail.com>
2024-06-13 10:03:22 +0530

    js file of L04 is added

---
+++ DAY_15_JS/L04_app.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators
+let a = 10; 
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+//Unary 
+console.log("Unary operator");
+console.log("post increment",a++);
+console.log("pre increment",++a);
+
+
+console.log("Assignment operator");
+b += a;
+console.log(a);
