56ecfee5c54936e78612416b59354692b8994610
Abhay Gupta <ab04012003@gmail.com>
2024-06-14 09:26:37 +0530

    js file of L05 is added

---
+++ DAY_15_JS/L05_app.js
@@ -0,0 +1,23 @@
+// Operators
+
+let age = 18;
+console.log("is age is greater than 18",age > 18);
+console.log("is age is greater or equal to 18",age >= 18);
+console.log(age < 18);
+console.log(age >= 18);
+console.log("age is equal to",age == 18);
+console.log("age is not equal to",age != 18);
+
+
+let n = 5;
+let str = "5";
+
+console.log(typeof n);
+typeof str;
+
+console.log(n==str);
+
+let str1 = "abhay";
+let str2 = "abhay";
+
+console.log(str1==str2);
