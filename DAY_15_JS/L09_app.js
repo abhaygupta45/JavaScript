10c81063c40eed4aa7bafae2c6ff0c6172a2c8ab
Abhay Gupta <ab04012003@gmail.com>
2024-06-17 09:55:40 +0530

    js file of L09 is added

---
+++ DAY_15_JS/L09_app.js
@@ -0,0 +1,13 @@
+// traffic light system.
+
+let color = "red";
+
+if (color == "green"){
+    console.log("Go");
+}
+if(color == "red"){
+    console.log("stop ! Traffic Light color is red");
+}
+if(color == "yellow"){
+    console.log("go slow");
+}
