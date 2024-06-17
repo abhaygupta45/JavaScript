c1aa8a6322667057c75a960d11f48ce6afb91b88
Abhay Gupta <ab04012003@gmail.com>
2024-06-17 09:53:52 +0530

    js file of L08 is added

---
+++ DAY_15_JS/L08_app.js
@@ -0,0 +1,26 @@
+// Conditional Statement
+
+// if Statement
+// some code before if
+
+// if(some condition){
+//     do something
+// }
+// some code after if
+
+console.log("Before my if statement...")
+let age = 21;
+if (age >= 18) {
+console.log("you can vote");
+console.log("you can drive");
+}
+if (age === 21){
+    console.log("You are in 20s!")
+}
+
+console.log("After my is Statement....")
+
+let firstName = "abhay";
+if(firstName == "abhay"){
+    console.log(`welcome ${firstName}`);
+}
