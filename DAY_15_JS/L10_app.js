720c1aecd489a8f1780cbc12039076284717cb8e
Abhay Gupta <ab04012003@gmail.com>
2024-06-18 07:59:21 +0530

    js file of L10 is added

---
+++ DAY_15_JS/L10_app.js
@@ -0,0 +1,31 @@
+// else if Statement
+
+// if (condition){
+//     do something
+// }
+// else if (conditon2){
+//     do something else
+// }
+// else if (condtion3){
+//     do something else
+// }
+
+let age = 14;
+if (age >= 18){
+    console.log("you can vote");
+} else if (age < 18){
+    console.log("you can not vote");
+}
+
+
+let marks = 32;
+
+if (marks >= 80){
+    console.log("A++");
+} else if (marks >= 60){
+    console.log("A ", "you are pass");
+}else if (marks >= 45){
+    console.log("B");
+}else if (marks < 33){
+    console.log("Fail 'L lag gye'");
+}
