2ff8d1e52434673cb177c4f84797d579148d5901
Abhay Gupta <ab04012003@gmail.com>
2024-06-19 09:28:20 +0530

    js file of L12 is added

---
+++ DAY_15_JS/L12_app.js
@@ -0,0 +1,16 @@
+// practice question 
+
+console.log("Enter the size of popcorn " ,  "'XL', 'L','M', 'S'")
+let size = "M";
+
+if (size === "XL"){
+    console.log("Price is Rs. 250");
+} else if (size === "L"){
+    console.log(" L size popcorn Price is Rs. 200");
+}else if (size === "M"){
+    console.log(" M size popcorn Price is Rs. 100");
+}else if (size === "S"){
+    console.log("price is Rs.50")
+}else{
+    console.log("Please enter size");
+}
