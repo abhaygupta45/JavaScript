24f3ebad2d4c0d8aefbb334f9917130ce3170bdf
Abhay Gupta <ab04012003@gmail.com>
2024-06-19 09:27:20 +0530

    js file of L11 is added

---
+++ DAY_15_JS/L11_app.js
@@ -0,0 +1,31 @@
+// else Statement
+
+// if (condtion){
+//     do something
+// }
+// else {
+//     do something
+// }
+
+let age = 18;
+
+if (age <= 18){
+    console.log("you can vote beeta");
+}else{
+    console.log("you can not vote");
+}
+
+// traffic light example
+let color = "blue";
+
+if (color == "green"){
+    console.log("Go");
+}
+else if(color == "red"){
+    console.log("stop ! Traffic Light color is red");
+}
+else if(color == "yellow"){
+    console.log("go slow");
+}else {
+    console.log("traffic light is broken")
+}
