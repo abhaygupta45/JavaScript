57285674c0bde734ea4fffb1a0eb24c135d81f15
Abhay Gupta <ab04012003@gmail.com>
2024-06-23 15:56:13 +0530

    js file of L18 is added

---
+++ DAY_15_JS/L18_app.js
@@ -0,0 +1,30 @@
+// use switch statement to print the day of the week.
+
+let day = 5;
+console.log("enter number between 1 to 7")
+
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wedmesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("satarday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Enter valid number");
+}
