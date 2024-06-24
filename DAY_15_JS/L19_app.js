2218a4a2fbef892bfdccbe8b6298bda1955d0476
Abhay Gupta <ab04012003@gmail.com>
2024-06-24 21:24:01 +0530

    js file of L19 is added

---
+++ DAY_15_JS/L19_app.js
@@ -0,0 +1,24 @@
+// alerts
+// alert("something is wrong");
+
+// console.log("This is a simple console log");
+
+// console.error("This is error");
+// console.warn("This is warning");
+
+
+// prompt//
+
+let firstName = prompt("enter your first name");
+let lastName = prompt("enter your last name");
+// console.log("Welcome",firstName,lastName, "!");
+
+let msg = "Welcome"+ " "+ firstName + " " + lastName + "!";
+
+alert(msg);
+
+// let roll = prompt("Enter your roll number : ");
+
+// console.log("your roll number is :" + roll);
+
+
