a17212c23bc0bf299f075796eb7e91f54e8d45c7
Abhay Gupta <ab04012003@gmail.com>
2024-06-28 10:56:09 +0530

    js file of L06 day 16 is added

---
+++ DAY_16_JS/L06_app.js
@@ -0,0 +1,10 @@
+let msg = "     hello    ";
+
+let newMsg = msg.trim();
+console.log("After Trim -",newMsg);
+
+newMsg = newMsg.toUpperCase();
+console.log("After upper case -",newMsg);
+
+// method chaining
+// let newMsg = msg.trim().toUpperCase()
