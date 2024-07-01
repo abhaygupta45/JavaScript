f3c57182ffc887b219031d5e2f56da57054040c4
Abhay Gupta <ab04012003@gmail.com>
2024-07-01 11:00:04 +0530

    js file of L10 is added

---
+++ DAY_16_JS/L10_app.js
@@ -0,0 +1,8 @@
+// Array(Data Structure)
+// array is the linear collection of things.
+
+// let student1 = "aman";
+// let student2 = "shradha";
+// let student3 = "abhay";
+
+let students = ["abhay","aman","shradha"];
