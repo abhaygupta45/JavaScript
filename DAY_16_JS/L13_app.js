d0e8e361d9ab69e2ea0fbf7c5e108e3bf138b5e1
Abhay Gupta <ab04012003@gmail.com>
2024-07-03 10:05:35 +0530

    js file of L13 is added

---
+++ DAY_16_JS/L13_app.js
@@ -0,0 +1,13 @@
+// Arrays are Mutable
+
+//  let name = "rohit"
+//  console.log(name);
+//  name [0] ='m'
+//  console.log(name);
+
+
+let fruits = ["mango", "apple", "litchi"];
+console.log(fruits);
+
+fruits[0] = "banana";
+fruits[10] = "papaya";
