bdefa849fbb714b75fd2bf8694784dee68793f3c
Abhay Gupta <ab04012003@gmail.com>
2024-07-04 17:11:57 +0530

    js file of L15 day 16 is added

---
+++ DAY_16_JS/L15_app.js
@@ -0,0 +1,17 @@
+// practice Question
+
+let month = ['january', 'july', 'march', 'august' ];
+
+console.log(month);
+
+month.shift()
+
+console.log(month);
+month.shift()
+console.log(month);
+
+month.unshift("june")
+console.log(month);
+
+month.unshift("july");
+console.log(month);
