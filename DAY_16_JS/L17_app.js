67c5ba9198d5bb99373cd7ed70bb88925d39a3ce
Abhay Gupta <ab04012003@gmail.com>
2024-07-05 09:54:35 +0530

    js file of L17 is added

---
+++ DAY_16_JS/L17_app.js
@@ -0,0 +1,13 @@
+// concat: merge 2 arrays
+
+let primary = ["red", "orange", "blue", "black"];
+
+let secondry = ["green","yellow", "grey"];
+
+console.log("After concatenation",primary.concat(secondry));
+
+
+// reverse method
+// reverse an array
+console.log("before reverse",primary);
+console.log("after reverse",primary.reverse());
