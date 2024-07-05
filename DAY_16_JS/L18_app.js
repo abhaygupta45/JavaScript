8c1203ca6363a3a91da3741f7ae8ecad8aadb367
Abhay Gupta <ab04012003@gmail.com>
2024-07-05 09:56:15 +0530

    js file of L18 is added

---
+++ DAY_16_JS/L18_app.js
@@ -0,0 +1,11 @@
+// slice method
+// it copies a portion of an array
+
+let colors = ["red","green","orange","white","black","blue" ];
+
+console.log("without using slice method : ",colors);
+console.log("with slice method : ",colors.slice());
+console.log("1 to 3: ",colors.slice(1,3));
+console.log("4: ",colors.slice(4));
+console.log("-2: ",colors.slice(-2  ));
+
