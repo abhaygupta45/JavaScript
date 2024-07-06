687e4a1a18d08d21e45584b1e4bc9df46343ad32
Abhay Gupta <ab04012003@gmail.com>
2024-07-06 22:05:46 +0530

    js file of L16 is added

---
+++ DAY_16_JS/L16_app.js
@@ -0,0 +1,13 @@
+console.log("indexOf and inclues methods");
+
+// indexOf : returns index of something
+
+let cars = ["bmw", "farari", "toyota", "maruti",];
+
+console.log(cars);
+cars.indexOf("toyota");
+console.log("indexOf toyota is",cars.indexOf("toyota"));
+
+// includes : search for a value.
+
+console.log("serach for for car which is exist or not in array",cars.includes("red"));
