98f1619e77300415e75f502113182cb3f648a862
Abhay Gupta <ab04012003@gmail.com>
2024-07-14 01:12:16 +0530

    js file of L02 dryrun is added

---
+++ DAY_17_JS/L02_dryRun_app.js
@@ -0,0 +1,16 @@
+console.log("dry run file of for loop");
+
+// for loop
+
+// for (initialisation; condition; updataion){
+//     do something
+// }
+
+for(let i=1; i<=5; i++){
+    console.log(i); 
+}
+
+console.log("backword conditon")
+for(let j=5; j>=1; j--){
+    console.log(j); 
+}
