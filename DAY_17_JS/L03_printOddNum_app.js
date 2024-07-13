eaef26f386c17f1b4bf7345d26165535dd9f11f6
Abhay Gupta <ab04012003@gmail.com>
2024-07-14 01:13:21 +0530

    js file of L03 is added

---
+++ DAY_17_JS/L03_printOddNum_app.js
@@ -0,0 +1,11 @@
+console.log("print all odd number (1 to 15) ");
+
+for (let i=1; i<=15; i= i+2){
+    console.log(i);
+}
+
+
+console.log("backword");
+for (let i=15; i>=1; i-=2){
+    console.log(i);
+}
