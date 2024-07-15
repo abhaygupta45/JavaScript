d25eaebb01154722cd25385d4e167b8131c07e35
Abhay Gupta <ab04012003@gmail.com>
2024-07-15 08:01:21 +0530

    js file of L04 day 17 is added

---
+++ DAY_17_JS/L04_printEvenNum_app.js
@@ -0,0 +1,16 @@
+console.log("print all even numbers (2 to)");
+
+for(let even=2; even<=10; even+=2){
+    console.log(even);
+}
+
+
+console.log("backword conditon");
+
+for(let even=10; even>=2; even-=2){
+    console.log(even);
+}
+
+for(let even=2; even<=109; even+=2){
+    console.log(even);
+}
