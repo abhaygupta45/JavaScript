b58497c00c296aa18b81060b9a7df7ca15f2ab8a
Abhay Gupta <ab04012003@gmail.com>
2024-07-16 10:02:58 +0530

    js file of L06 is added

---
+++ DAY_17_JS/L06_printMultiplicationTable_app.js
@@ -0,0 +1,17 @@
+console.log("print the multiplication table for 5");
+
+//normal zindgi
+// for (let i=5; i<=50; i+=5){
+//     console.log(i)
+// }
+
+//mentos zindgi
+
+// console.log("write the taking input from user");
+ 
+let num = prompt("Write your Number");
+num = parseInt(num);
+
+for (let i=num; i<=num*10; i= i+num){
+    console.log(i);
+}
