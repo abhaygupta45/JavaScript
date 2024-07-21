39057570088ff6dddb78887f5dd47ec7436b8988
Abhay Gupta <ab04012003@gmail.com>
2024-07-21 13:59:38 +0530

    js file of L13 is added

---
+++ DAY_17_JS/L13_forOfLoops_app.js
@@ -0,0 +1,15 @@
+// for of loop
+
+// for (element of collection){
+//     do something
+// }
+
+let fruits = ["mango", "apple", "banana", "orange", "litchi" ];
+
+for (fruit of fruits){
+    console.log(fruits);
+}
+
+for (char of "apnacollege"){
+    console.log(char);
+}
