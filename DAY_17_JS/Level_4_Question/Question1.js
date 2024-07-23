38c24673299061edb28ae1d138c15129201a20fe
Abhay Gupta <ab04012003@gmail.com>
2024-07-23 17:40:23 +0530

    js file of question 1 is added

---
+++ DAY_17_JS/Level_4_Question/Question1.js
@@ -0,0 +1,15 @@
+// Write a JS program to delete all occurrences of element 'num' in a given array.
+// example : if arr = [1,2,3,4,5,6,2,3,] & num = 2
+// result should be array = [1,3,4,5,6,3] 
+
+
+let arr = [1,2,3,4,5,6,2,3];
+num = 2;
+
+for (let i = 0; i<arr.length; i++){
+    if (arr[i] == num){
+        arr.splice(i,1);
+    }
+}
+
+console.log(arr);
