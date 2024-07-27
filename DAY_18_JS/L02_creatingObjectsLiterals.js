57094291758a330fefb5658e94ef4703956e7023
Abhay Gupta <ab04012003@gmail.com>
2024-07-27 07:30:11 +0530

    js file of L02 is added

---
+++ DAY_18_JS/L02_creatingObjectsLiterals.js
@@ -0,0 +1,14 @@
+const student = {
+    name : "Abhay",
+    age : 20,
+    marsk : 92.5
+};
+console.log(student);
+
+const item = {
+    price : 1000.4,
+    discount : 500,
+    colors : ["red", "pink"]
+};
+
+console.log(item);
