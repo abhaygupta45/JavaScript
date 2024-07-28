9be33f5ac69fedcd04b9b75b169a35bd0fd96ad9
Abhay Gupta <ab04012003@gmail.com>
2024-07-28 09:05:58 +0530

    js file of L04 is added

---
+++ DAY_18_JS/L04_getValues.js
@@ -0,0 +1,23 @@
+// Get values
+// let student = {
+//     name : "Abhay",
+//     marks : 85.6
+// };
+
+// student["name"]
+// student.name
+
+const post = {
+    username : "@abhaygupta",
+    content : "This is my #first post",
+    likes : 150,
+    reposts : 5,
+    tags : ["@apnacollege", "delta"]
+};
+
+console.log(post.username);
+console.log(post.content);
+console.log(post.likes);
+
+console.log(post["reposts"]);
+
