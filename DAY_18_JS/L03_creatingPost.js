6e8593ee9e7dddc7dac14f5cb0955f5864b5bdb6
Abhay Gupta <ab04012003@gmail.com>
2024-07-28 09:05:03 +0530

    js file of L03 is added

---
+++ DAY_18_JS/L03_creatingPost.js
@@ -0,0 +1,19 @@
+// Thread / twitter post
+
+// create an object literal for the properties of thread/ twitter post which inclues-
+
+// username
+// content
+// likes
+// reposts
+// tags
+
+const post = {
+    username : "@abhaygupta",
+    content : "This is my #first post",
+    likes : 150,
+    reposts : 5,
+    tags : ["@apnacollege", "delta"]
+};
+
+console.log(post);
