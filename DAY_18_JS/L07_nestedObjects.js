bfc7c05db06d3e604915dfd8a5e3e9187e5a87cf
Abhay Gupta <ab04012003@gmail.com>
2024-07-30 08:54:03 +0530

    js file of L07 is added

---
+++ DAY_18_JS/L07_nestedObjects.js
@@ -0,0 +1,22 @@
+// Objects of Objects
+// storing information of multiple students 
+
+console.log("nested objects");
+
+const classInfo = {
+    aman : {
+        grade : "A+",
+        city : "Delhi"
+    },
+    abhay : { 
+        grade : "A++++++",
+        city : "noida"
+    },
+    chhavi : {
+        grade : "0",
+        city : "mumbai"
+    }
+};
+
+console.log(classInfo);
+
