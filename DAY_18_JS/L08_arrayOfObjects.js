a8f3683823a8e10c442543810dc67990eb3cd49e
Abhay Gupta <ab04012003@gmail.com>
2024-07-31 08:58:19 +0530

    js file of L08 is added

---
+++ DAY_18_JS/L08_arrayOfObjects.js
@@ -0,0 +1,22 @@
+// Array of objects
+// Storing information of multiple students.Array
+
+const classInfo = [
+    {
+        name : "abhay",
+        grade : "A++",
+        city : "noida"
+    },
+    {
+        name : "sachin",
+        grade : "A",
+        city : "mumbai"
+    },
+    {
+        name : "Chhavi",
+        grade : "A+",
+        city : "delhi"
+    }  
+];
+
+console.log(classInfo);
