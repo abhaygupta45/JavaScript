59c3917a732c6c28c3b1f66c7db1370bb618244d
Abhay Gupta <ab04012003@gmail.com>
2024-08-01 06:43:08 +0530

    js file of L10 day 18 is added

---
+++ DAY_18_JS/L10_randomIntegers.js
@@ -0,0 +1,16 @@
+// Random Integers
+// from 1 to 10 
+
+let step1 = Math.random();
+console.log(step1);
+let step2 = step1 * 10;
+console.log(step2);
+let step3 = Math.floor(step2)
+console.log(step3);
+
+
+console.log(Math.floor(Math.random()*10));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+// Math.floor(Math.random()*10)
