a8e9ef7cbb1177d2514834c6eeefd2abcbf658af
Abhay Gupta <ab04012003@gmail.com>
2024-08-01 06:41:51 +0530

    js file of L08 day 18 is added

---
+++ DAY_18_JS/L09_mathObjects.js
@@ -0,0 +1,37 @@
+// Math Object
+
+// properties            methos
+
+// Math.PI               Math.abs(n)
+// Math.E                Math.pow(a,b)
+//                       Math.floor(n)
+//                       Math.cell(n)
+//                       Math.random()
+
+// Math.abs(n)
+Maths.abs(-3);
+122
+
+// Math.pow(a,b)
+Math.pow(2,4)
+16
+
+// Math.floor(n)
+Math.floor(247)
+247
+Math.floor(5.6)
+5
+Math.floor(-5.3)
+-6
+
+// Math.cell(n)
+Math.ceil(5.1)
+6
+Math.ceil(-55)
+-55
+
+// Math.random()
+Math.random()
+0.525891180659311
+Math.random()
+0.5027223980110096
