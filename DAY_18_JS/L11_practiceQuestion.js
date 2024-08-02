ea5daec23dcba8b23586ba24bb30958d4ec3dde7
Abhay Gupta <ab04012003@gmail.com>
2024-08-02 08:14:16 +0530

    js file of l11 day 18 is added

---
+++ DAY_18_JS/L11_practiceQuestion.js
@@ -0,0 +1,21 @@
+//Generate a random number between 1 to 100
+
+let step1 = Math.random();
+console.log(step1);
+let step2 = step1 * 100;
+console.log(step2);
+let step3 = Math.floor(step2)
+console.log(step3);
+let step4 = step3 + 1;
+console.log(step4);
+
+
+
+console.log(Math.floor(Math.random()*100));
+
+console.log(Math.floor(Math.random()*100)+1);
+
+
+// Generate a random number between 1 to 5
+
+console.log(Math.floor(Math.random()*5)+1);
