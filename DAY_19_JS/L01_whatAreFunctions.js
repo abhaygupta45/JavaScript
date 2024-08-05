aa62a9fd675ddbf06a6df2ea916d676a52aecf21
Abhay Gupta <ab04012003@gmail.com>
2024-08-05 08:33:02 +0530

    js file of L01 day 19 is added

---
+++ DAY_19_JS/L01_whatAreFunctions.js
@@ -0,0 +1,29 @@
+// Functions in JS
+
+// function definition (telling JS)
+
+// function funcName(){
+//     do something
+// }
+
+// function calling (using the function)
+
+// funcName();
+
+function printName() {
+    console.log("Abhay Gupta");
+    console.log("Sage university bhopal")
+}
+
+printName();
+
+
+console.log("print 1 to 5 using function");
+
+function print1to5(){
+    for(let i = 1; i<=5; i++){
+        console.log(i);
+    }
+}
+
+print1to5();
