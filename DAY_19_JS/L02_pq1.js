0427a5800019e5192655ef89eb2b1814af4acd4d
Abhay Gupta <ab04012003@gmail.com>
2024-08-06 08:11:22 +0530

    js file of L02 is added

---
+++ DAY_19_JS/L02_pq1.js
@@ -0,0 +1,9 @@
+console.log("create a function that prints a poem.");
+
+function printPoem(){
+    console.log("Twinkle Twinkle, little star");
+    console.log("how i wonder what you are");
+}
+
+// calling function
+printPoem();
