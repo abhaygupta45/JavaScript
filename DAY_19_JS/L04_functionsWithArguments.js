e09d1eab363bdf4d2c172b05c871d34c861f96cd
Abhay Gupta <ab04012003@gmail.com>
2024-08-07 16:13:05 +0530

    js file of L04 is added

---
+++ DAY_19_JS/L04_functionsWithArguments.js
@@ -0,0 +1,20 @@
+// Function with Arguments
+// Value we pass to the function
+
+// function funcName (arg1, arg2....){
+//     do something
+// }
+
+// funcName();
+
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}.`)
+}
+
+printInfo("Abhay",20);
+
+function printSum (a,b){
+    console.log(`sum of ${a} and ${b} is`,a+b)
+}
+
+printSum(3,4);
