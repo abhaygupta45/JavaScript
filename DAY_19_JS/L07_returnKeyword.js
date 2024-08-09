cba614267c9051615b56e757f916c3a4e0b18c74
Abhay Gupta <ab04012003@gmail.com>
2024-08-09 09:51:07 +0530

    js file of L07 day 19 is added

---
+++ DAY_19_JS/L07_returnKeyword.js
@@ -0,0 +1,28 @@
+// Return 
+// return keyword is used to return some value from the function.
+
+// input-----> function(does some work) ------> output
+
+// function funcName(arg1, arg2....){
+//     do something
+//     return val;
+// }
+
+console.log ("function that calculate two number of sum with return keyword");
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(3,4));
+
+
+console.log ("function that calculate THREE number of sum with return keyword");
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(sum(sum(2,4),3));
+
+
