01ee25a3e2c6f021dcf7db9f4c0d7f23f18bbb1b
Abhay Gupta <ab04012003@gmail.com>
2024-08-15 19:46:55 +0530

    js file of L16 day 19 is added

---
+++ DAY_19_JS/L16_higherOrderFunctionReturns.js
@@ -0,0 +1,15 @@
+function oddEvenFactory(request){
+    if(request == "odd"){
+        return function (n){
+            console.log(!(n%2 == 0));
+        }
+    }else if (request == "even"){
+        return function (n){
+            console.log(n%2 == 0);
+        }
+    }else {
+        console.log("Wrong request");
+    }
+}
+
+let request = "odd" // even
