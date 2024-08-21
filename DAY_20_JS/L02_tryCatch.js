ea1e0c17d66adc4d596066b6e92afb00834ce9f7
Abhay Gupta <ab04012003@gmail.com>
2024-08-21 10:30:41 +0530

    js file of day 20 L02 is added

---
+++ DAY_20_JS/L02_tryCatch.js
@@ -0,0 +1,15 @@
+//The try statement allows you to define a block of code to be tested for errors while it is being executed.
+//The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+
+console.log("hello");
+console.log("hello1");
+// let a = 4;
+try {
+    console.log(a);
+}catch {
+    console.log("caught an error .... a is not defined");
+    console.log(err);
+}
+console.log("hello2");
+console.log("hello3");
+console.log("hello4");
