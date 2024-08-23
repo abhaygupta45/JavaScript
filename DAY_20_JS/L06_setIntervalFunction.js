d1b2883a63966f9ae87d7347c28b3f8c2c644c22
Abhay Gupta <ab04012003@gmail.com>
2024-08-23 12:00:03 +0530

    js  file of day 20 L06 is added

---
+++ DAY_20_JS/L06_setIntervalFunction.js
@@ -0,0 +1,19 @@
+// Set Interval
+// setInterval (function, timeout)
+
+// setInterval (function,timeout)
+
+let id = setInterval (() => {
+    console.log("Apna College");
+}, 2000);
+
+console.log(id);
+
+let id2 = setInterval (() => {
+    console.log("Hello World");
+}, 4000);
+
+console.log(id2);
+
+ clearInterval(id)
+ clearInterval(id2)
