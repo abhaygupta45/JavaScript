7596ced858c6288e7941199c5d25f06621437f25
Abhay Gupta <ab04012003@gmail.com>
2024-08-23 11:59:01 +0530

    js file of L05 day 20 is added

---
+++ DAY_20_JS/L05_setTimeOutFunction.js
@@ -0,0 +1,13 @@
+// Set Timeout 
+// setTimeout (function, timeout)
+
+
+// setTimeout(function,timeout)
+
+console.log("Hi there !");
+
+setTimeout(() => {
+    console.log("magic magic magic");
+},4000)
+
+console.log("wiat wait wait !");
